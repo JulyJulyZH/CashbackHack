3d541e8f2ffc8f7975519d14940a7ab433049b06
Jullila <zharovayulya1997@gmail.com>
2023-10-14 16:17:36 +0500

    JUnit4-Platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
 
@@ -40,5 +42,41 @@ public class CashbackHackServiceTest {
         int expected = 1000;
         int actual = cashbackHackService.remain(amount);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainLessThenBoundaryAPI() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        final int amount = 400;
+        int expected = 600;
+        int actual = cashbackHackService.remain(amount);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainMoreThenBoundaryAPI() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        final int amount = 1500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainEqualsBoundaryAPI() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        final int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainZeroAPI() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        final int amount = 0;
+        int expected = 1000;
+        int actual = cashbackHackService.remain(amount);
+        Assertions.assertEquals(expected, actual);
     }
 }
